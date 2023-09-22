366e660217fa9494c82fdeaed4fe41bdafd478cb
ian-bravo <bravo.ian@gmail.com>
2023-09-22 14:35:03 -0700

    pass price-of-loaves-no-discount test

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -7,11 +7,19 @@ namespace Bakery.Tests
   public class BreadTests
   {
     [TestMethod]
-    public void LoafCalculator_CalculatesOfEnteredLoaves_NumberOfLoaves()
+    public void LoafCalculator_CalculatesTheEnteredLoaves_NumberOfLoaves()
     {
       string userInputForNumberOfLoaves = "1";
       int result = Bread.LoafCalculator(userInputForNumberOfLoaves);
       Assert.AreEqual(1, result);
+    }
+
+    [TestMethod]
+    public void LoafCalculator_CalculatesLoafCostNoDiscount_CostOfLoaves()
+    {
+      int numberOfLoaves = 2;
+      int priceOfLoaves = Bread.LoafCalculator(numberOfLoaves);
+      Assert.AreEqual(10, priceOfLoaves);
     }
   }
 }
--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -4,10 +4,17 @@ namespace Bakery.Models
 {
   public class Bread
   {
+
     public static int LoafCalculator(string userEnteredNumberOfLoaves)
     {
       int returnsStringAsInt = int.Parse(userEnteredNumberOfLoaves);
       return returnsStringAsInt;
+    }
+
+    public static int LoafCalculator(int userEnterAsInt)
+    {
+      int priceOfLoaves = userEnterAsInt * 5;
+      return priceOfLoaves;
     }
   }
 }
